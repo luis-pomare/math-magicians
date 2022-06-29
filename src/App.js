c5110df1243b18ca91f4e53c22b32e2184fd72a9
unknown <luispomare92@gmail.com>
2022-06-29 15:07:21 -0500

    Refactor app.js to use functional component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 // eslint-disable-next-line
 import Calculator from './components/calculator.js';
 
-// eslint-disable-next-line
-class App extends React.Component {
-  // eslint-disable-next-line
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
