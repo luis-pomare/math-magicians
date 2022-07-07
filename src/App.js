f1d7015c360d5aa939d375e3cba2ba986444cad0
unknown <luispomare92@gmail.com>
2022-07-07 12:16:59 -0500

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/calculator';
+import Home from './components/home';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <h1>Math Magicians</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
